a5908ae375211dd344c37b5dd1909232af7c910c
benbj_yang <benbj_yang@asus.com>
2021-02-23 11:37:53 +0800

    update receving function of ReceiveRPC

--- msgqueue/common/rpc.go
+++ msgqueue/common/rpc.go
@@ -4,6 +4,9 @@ type (
 	// RPCInterface ...
 	RPCInterface interface {
 		SendRPC(name, rKey, replyTo, payload string) (res string, err error)
-		ReceiveRPC(name string, rec Receiving) (err error)
+		ReceiveRPC(name string, del Delivering) (err error)
 	}
+
+	// Delivering return data from server side of rpc mode
+	Delivering func(messageIDs string, headers map[string]interface{}, body []byte) (data []byte)
 )
--- msgqueue/rabbitmq/receive_rpc.go
+++ msgqueue/rabbitmq/receive_rpc.go
@@ -1,38 +1,21 @@
 package rabbitmq
 
 import (
-	"errors"
-	"time"
-
 	"github.com/pegasus-cloud/msgqueue_client/msgqueue/common"
 )
 
 // ReceiveRPC receive rpc message
-func (q *QueueMethod) ReceiveRPC(name string, rec common.Receiving) (err error) {
+func (q *QueueMethod) ReceiveRPC(name string, del common.Delivering) (err error) {
 
 	msgs, err := q.Provider.AMQP.Consume(name, 1)
-	select {
-	case d := <-msgs:
-		rec(d.CorrelationId, d.Headers, d.Body, nil)
-		if err := q.Provider.AMQP.PublishRPC("", d.ReplyTo, "", d.CorrelationId, Success); err != nil {
+	if err != nil {
+		return err
+	}
+	for d := range msgs {
+		if err := q.Provider.AMQP.PublishRPC("", d.ReplyTo, "", d.CorrelationId, string(del(d.CorrelationId, d.Headers, d.Body))); err != nil {
 			return err
 		}
-	case <-chTimeout(q.Provider.AMQP.RPCTimeout):
-		return errors.New("amqp rpc timeout")
-	default:
-		return errors.New("unexpected error")
 	}
 
 	return nil
-}
-
-func chTimeout(timeout int) chan bool {
-	tch := make(chan bool, 1)
-
-	go func() {
-		time.Sleep(time.Duration(timeout) * time.Second)
-		tch <- true
-	}()
-
-	return tch
 }
